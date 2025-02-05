d35c11c05fae6ed483e356cb8c8c8080a1bb01fe
DamienGilliard <damien.gilliard@epfl.ch>
2025-02-05 22:05:09 +0100

    feat: add basic file iterator and Coordinate units conversions

---
+++ src/Referee/Utils/Utils.cc
@@ -0,0 +1,56 @@
+#include "Utils.hh"
+
+namespace Referee::Utils
+{
+    namespace Conversions
+    {
+        void ConvertLatLonAltToCartesian(double lat, double lon, double alt, double &x, double &y, double &z, CoordinateSystem fromCoordSys, CoordinateSystem toCoordSys)
+        {
+            if(fromCoordSys == CoordinateSystem::DEG && toCoordSys == CoordinateSystem::LV95)
+            {
+                // according to https://backend.swisstopo.admin.ch/fileservice/sdweb-docs-prod-swisstopoch-files/files/2023/11/14/7f7bf15b-22e2-48b6-b1ab-6905f81dca8a.pdf
+                // the conversion from WGS84 to CH1903/LV95 is done by the following formulas
+                // conversion from long lat to arcsec:
+                double latArcSec = lat * 3600;
+                double lonArcSec = lon * 3600;
+                // calculation of auxillary values
+                double phi = (latArcSec - 169028.66) / 10000;
+                double lambda = (lonArcSec - 26782.5) / 10000;
+                // calculation of the coordinates
+                y = 2600072.37 + 211455.93 * lambda - 10938.51 * lambda * phi - 0.36 * lambda * phi * phi - 44.54 * lambda * lambda * lambda;
+                x = 1200147.07 + 308807.95 * phi + 3745.25 * lambda * lambda + 76.63 * phi * phi - 194.56 * lambda * lambda * phi + 119.79 * phi * phi * phi;
+
+                z = alt - 49.55 + 2.73 * lambda + 6.94 * phi;
+            }
+            else if (fromCoordSys == CoordinateSystem::DMS && toCoordSys == CoordinateSystem::LV95)
+            {
+                // convert DMS to DEG
+                double convertedDegrees = int(lat);
+                double convertedMinutes = int((lat - convertedDegrees) * 100);
+                double convertedSeconds = (lat - convertedDegrees - convertedMinutes / 100) * 10000;
+                double latDeg = convertedDegrees + convertedMinutes/60 + convertedSeconds/3600; ;
+
+                convertedDegrees = int(lon);
+                convertedMinutes = int((lon - convertedDegrees) * 100);
+                convertedSeconds = (lon - convertedDegrees - convertedMinutes / 100) * 10000;
+                double lonDeg = convertedDegrees + convertedMinutes/60 + convertedSeconds/3600;
+
+                ConvertLatLonAltToCartesian(latDeg, lonDeg, alt, x, y, z, CoordinateSystem::DEG, CoordinateSystem::LV95);
+            }
+            
+        }
+    }
+    namespace FileIterators
+    {
+        std::vector<std::string> GetFilesInDirectory(const std::string& directory, const std::string& extension)
+        {
+            std::vector<std::string> files;
+            for (const auto& entry : std::filesystem::directory_iterator(directory)) {
+                if (entry.path().extension() == extension) {
+                    files.push_back(entry.path().string());
+                }
+            }
+            return files;
+        }
+    }
+}
---
+++ src/Referee/Utils/Utils.hh
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <sstream>
+
+namespace Referee 
+{
+    namespace Utils 
+    {
+        enum class CoordinateSystem
+        {
+            UTM,    //Universal Transverse Mercator 
+            DMS,    //Degrees, Minutes, Seconds
+            DEG,    //Decimal Degrees
+            LV95,   //Swiss Grid (translation of UTM)
+        };
+        /*
+        @brief Convert latitude, longitude to Cartesian coordinates (X, Y)
+        @param lat Latitude in degrees
+        @param lon Longitude in degrees
+        @param x X coordinate in meters
+        @param y Y coordinate in meters
+        @param FromCoordSys Coordinate system of input latitude and longitude
+        @param ToCoordSys Coordinate system of output coordinates
+        */
+        namespace Conversions
+        {
+                void ConvertLatLonAltToCartesian(double lat, double lon, double alt, double &x, double &y, double &z, CoordinateSystem fromCoordSys, CoordinateSystem toCoordSys);
+                void ConvertECEFToLatLonAlt(double x, double y, double z, double &lat, double &lon, double &alt);
+        }
+        namespace FileIterators
+        {
+            /*
+            @brief Get all files in a directory with a specific extension
+            @param directory Directory to search for files
+            @param extension File extension to search for
+            */
+            std::vector<std::string> GetFilesInDirectory(const std::string& directory, const std::string& extension);
+        }
+    }
+}
