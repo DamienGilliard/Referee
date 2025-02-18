6e24eca1269850281b514225de0c62e3d23a9929
damien <damien.gilliard@epfl.ch>
2025-02-18 13:03:38 +0100

    fix: include necessary headers

--- src/Referee/Visualisation/Visualisation.hh
+++ src/Referee/Visualisation/Visualisation.hh
@@ -8,11 +8,10 @@
 #include <thread>
 #include <chrono>
 
-#include "pcl/io/ply_io.h"
-#include "pcl/point_types.h"
-#include "pcl/visualization/pcl_visualizer.h"
+#include <pcl/point_types.h>
 #include <pcl/common/centroid.h>
-
+#include <pcl/io/ply_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
 namespace FileBasedVisualisation
 {
