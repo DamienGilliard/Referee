57bbdb1a766c6082a80a4f566e61f6303e2f91bd
DamienGilliard <damien.gilliard@epfl.ch>
2025-03-23 21:47:33 +0100

    feat: add MappingMatrix class to store transformations between point clouds + implementaation of GlobalMatch registration

--- src/Referee/Mapping/Mapping.cc
+++ src/Referee/Mapping/Mapping.cc
@@ -2,13 +2,26 @@
 
 namespace Referee::Mapping
 {
-    void CreateConnectivityGraph(std::vector<std::vector<double>> geolocations, int knn, double maxDistance, std::vector<std::vector<int>>& graph)
+    void MappingMatrix::PrintMatrix()
     {
-        if(graph.size() != geolocations.size())
+        for(int i = 0; i < __mappingMatrix.size(); i++)
         {
-            graph.resize(geolocations.size());
+            for(int j = 0; j < __mappingMatrix[i].size(); j++)
+            {
+                std::cout << __mappingMatrix[i][j] << " ";
+            }
+            std::cout << std::endl;
         }
-        
+    }
+
+    void CreateConnectivityMatrix(std::vector<std::vector<double>> geolocations, int knn, double maxDistance, std::vector<std::vector<int>>& matrix)
+    {
+        std::vector<std::vector<double>> distances;
+        std::vector<std::vector<int>> totalMatrix;
+        distances.resize(geolocations.size());
+        totalMatrix.resize(geolocations.size());
+        matrix.resize(geolocations.size());
+
         for(int i = 0; i < geolocations.size(); i++)
         {
             for(int j = 0; j < geolocations.size(); j++)
@@ -16,20 +29,52 @@ namespace Referee::Mapping
                 if(i != j)
                 {
                     // Calculate distance between geolocations
-                    double distance = 0;
-                    for(int k = 0; k < 3; k++)
-                    {
-                        distance += pow(geolocations[i][k] - geolocations[j][k], 2);
-                    }
+                    double distance = std::pow(geolocations[i][0] - geolocations[j][0], 2) + std::pow(geolocations[i][1] - geolocations[j][1], 2) + std::pow(geolocations[i][2] - geolocations[j][2], 2);
                     distance = sqrt(distance);
-
-                    // Check if distance is within the maximum distance
-                    if(distance < maxDistance)
-                    {
-                        graph[i].push_back(j);
-                    }
+                    distances[i].push_back(distance);
+                    totalMatrix[i].push_back(j);
                 }
             }
         }
+
+        // Get the knn closest neighbors for each node
+        for(int i = 0; i < totalMatrix.size(); i++)
+        {
+            std::vector<int> neighbors = totalMatrix[i];
+            std::sort(neighbors.begin(), neighbors.end(), [&distances, i](int a, int b) { return distances[i][a] < distances[i][b]; });
+            matrix[i].resize(knn);
+            for(int j = 0; j < knn; j++)
+            {
+                matrix[i][j] = neighbors[j];
+            }
+        }
+    }
+
+    Eigen::Matrix4f ComputePairwiseTransformation(pcl::PointCloud<pcl::PointXYZ>::Ptr source, pcl::PointCloud<pcl::PointXYZ>::Ptr target, TransformationComputationMethod method)
+    {
+        Eigen::Matrix4f transformation = Eigen::Matrix4f::Identity();
+
+        if(method == TransformationComputationMethod::GlobalMatch)
+        {
+            // Compute transformation using GlobalMatch, currently a copy of the main function from GlobalMatch's main.cpp
+            std::cout << "Computing transformation using GlobalMatch" << std::endl;
+            GlobalMatch::Mapping::Mapping globalMatchMapping;
+            pcl::PointCloud<pcl::PointXYZ>::Ptr sourcePosCloud(new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::PointCloud<pcl::PointXYZ>::Ptr targetPosCloud(new pcl::PointCloud<pcl::PointXYZ>);
+            globalMatchMapping.setInputCloud(source->makeShared());
+            globalMatchMapping.extract(sourcePosCloud);
+            globalMatchMapping.setInputCloud(target->makeShared());
+            globalMatchMapping.extract(targetPosCloud);
+            
+            GlobalMatch::Matching::Matching globalMatchMatching;
+            globalMatchMatching.setPairwiseStemPositions(sourcePosCloud, targetPosCloud);
+            globalMatchMatching.estimateTransformation(transformation);
+        }
+        else
+        {
+            std::cerr << "Unknown transformation computation method" << std::endl;
+        }
+
+        return transformation;
     }
 }
--- src/Referee/Mapping/Mapping.hh
+++ src/Referee/Mapping/Mapping.hh
@@ -2,16 +2,95 @@
 #include <vector>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <Eigen/Dense>
+#include "../../3rd_party/GlobalMatch/code/global_match/stem_mapping.h"
+#include "../../3rd_party/GlobalMatch/code/global_match/stem_matching.h"
 
 namespace Referee::Mapping
 {
+
+    
     /**
-     * @brief Create a connectivity graph from a set of geolocations
+     * @brief MappingMatrix class to store the N x N mapping matrix `M` between N point clouds, where each element M_{i,j} is the 4x4 transformation matrix between point cloud i and point cloud j
+     */
+    class MappingMatrix
+    {
+    public:
+        /**
+         * @brief Construct a new MappingMatrix object
+         * @param numPointClouds Number of point clouds
+         */
+        MappingMatrix(int numPointClouds)
+        {
+            __mappingMatrix.resize(numPointClouds);
+            for (int i = 0; i < numPointClouds; i++)
+            {
+                __mappingMatrix[i].resize(numPointClouds);
+            }
+        }
+
+        /**
+         * @brief Getter of the transformation matrix between two point clouds
+         * @param i Index of the first point cloud
+         * @param j Index of the second point cloud
+         * @return Eigen::Matrix4f Transformation matrix between the two point clouds
+         */
+        Eigen::Matrix4f GetTransformationMatrix(int i, int j)
+        {
+            return __mappingMatrix[i][j];
+        }
+
+        /**
+         * @brief Setter for the transformation matrix between two point clouds
+         * @param i Index of the first point cloud
+         * @param j Index of the second point cloud
+         * @param transformationMatrix Transformation matrix between the two point clouds
+         */
+        void SetTransformationMatrix(int i, int j, Eigen::Matrix4f transformationMatrix)
+        {
+            __mappingMatrix[i][j] = transformationMatrix;
+        }
+
+        std::vector<std::vector<int>> GetConnectivityMatrix();
+
+        /**
+         * @brief Print the mapping matrix to the console
+         */
+        void PrintMatrix();
+
+    private:
+        /**
+         * @brief Mapping matrix between the point clouds, where each element is the transformation matrix between two point clouds
+         */
+        std::vector<std::vector<Eigen::Matrix4f>> __mappingMatrix;
+    };
+    
+
+
+    /**
+     * @brief Create a connectivity matrix from a set of geolocations
      * @param geolocations Geolocations of the nodes
      * @param knn Number of nearest neighbors to consider
      * @param maxDistance Maximum distance to consider a connection
-     * @param graph Connectivity graph to be created. Each element of the vector is the list of indices the respective geolocations are connected to
+     * @param matrix Connectivity matrix to be created. Each element of the vector is the list of indices the respective geolocations are connected to
      */
-    void CreateConnectivityGraph(std::vector<std::vector<double>> geolocations, int knn, double maxDistance, std::vector<std::vector<int>>& graph);
+    void CreateConnectivityMatrix(std::vector<std::vector<double>> geolocations, int knn, double maxDistance, std::vector<std::vector<int>>& matrix);
     
+
+
+    /**
+     * @brief enum to store the different transformation calculation methods. Currently only GlobalMatch is supported: https://doi.org/10.1016/j.isprsjprs.2023.01.013 
+    */
+    enum TransformationComputationMethod
+    {
+        GlobalMatch,
+    };
+
+    /**
+     * @brief Computes the 4x4 transformation matrix that transforms the source point cloud to the target point cloud following a given method
+     * @param source the source point cloud used as reference
+     * @param target the point cloud we want to transform
+     * @param method the method used in the computation.  
+    */
+    Eigen::Matrix4f ComputePairwiseTransformation(pcl::PointCloud<pcl::PointXYZ>::Ptr source, pcl::PointCloud<pcl::PointXYZ>::Ptr target, TransformationComputationMethod method);
 }
