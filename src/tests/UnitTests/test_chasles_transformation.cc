2d69dcb181c5f075c967d1a6dcf425275f2fbbde
Damien Gilliard <damien.gilliard@epfl.ch>
2025-04-09 16:32:36 +0200

    feat: add test for plane intersection

--- src/tests/UnitTests/test_chasles_transformation.cc
+++ src/tests/UnitTests/test_chasles_transformation.cc
@@ -5,17 +5,19 @@
 
 int main()
 {
-    Eigen::AngleAxisd angleAxis( M_PI / 4, Eigen::Vector3d(0, 0, 1));
+    Eigen::AngleAxisd angleAxis( M_PI / 2, Eigen::Vector3d(0, 0, 1));
     Eigen::Matrix3d rotationMatrix = angleAxis.toRotationMatrix();
+    Eigen::Vector3d translationVector(100, 0, 0);
     Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
     transformationMatrix.block<3, 3>(0, 0) = rotationMatrix;
+    transformationMatrix.block<3, 1>(0, 3) = translationVector;
 
     Referee::Mapping::ChaslesTransformation chaslesTransformation(transformationMatrix);
     Eigen::Vector3d rotationAxis = chaslesTransformation.GetRotationAxis();
     Eigen::Vector3d pointOnAxis = chaslesTransformation.GetPointOnAxis();
     double rotationAngle = chaslesTransformation.GetRotationAngle();
 
-    if(rotationAxis.isApprox(Eigen::Vector3d(0, 0, M_PI / 4), 1e-6) && pointOnAxis.isApprox(Eigen::Vector3d(0, 0, 0), 1e-6) && std::abs(rotationAngle - M_PI / 4) < 1e-6)
+    if(rotationAxis.isApprox(Eigen::Vector3d(0, 0, M_PI / 2), 1e-6) && pointOnAxis.isApprox(Eigen::Vector3d(50, 50, 0), 1e-6) && std::abs(rotationAngle - M_PI / 2) < 1e-6)
     {
         std::cout << "Test passed: Chasles transformation is correct" << std::endl;
         return 0;
@@ -23,6 +25,12 @@ int main()
     else
     {
         std::cout << "Test failed: Chasles transformation is not correct" << std::endl;
+        std::cout << "Rotation axis: " << rotationAxis.transpose() << std::endl;
+        std::cout << "Point on axis: " << pointOnAxis.transpose() << std::endl;
+        std::cout << "Rotation angle: " << rotationAngle << std::endl;
+        std::cout << "Expected rotation axis: " << Eigen::Vector3d(0, 0, M_PI / 2).transpose() << std::endl;
+        std::cout << "Expected point on axis: " << Eigen::Vector3d(50, 50, 0).transpose() << std::endl;
+        std::cout << "Expected rotation angle: " << M_PI / 4 << std::endl;
         return 1;
     }
     
