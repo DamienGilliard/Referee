a02478aaf2f6e077c67ef4c964e8f8e33547c323
Damien Gilliard <damien.gilliard@epfl.ch>
2025-04-10 10:45:36 +0200

    feat: implement switch from 4x4 matrices to Chesles transformations in tests

--- src/tests/UnitTests/test_mean_transformation.cc
+++ src/tests/UnitTests/test_mean_transformation.cc
@@ -1,40 +1,41 @@
 #include "../../Referee.hh"
 #include <Eigen/Dense>
+#include <cmath>
 int main()
 {
     Eigen::Matrix3d rotationMatrix = Eigen::AngleAxisd(M_PI / 4, Eigen::Vector3d::UnitZ()).toRotationMatrix();
     Eigen::Matrix3d inverseRotationMatrix = Eigen::AngleAxisd(-M_PI / 4, Eigen::Vector3d::UnitZ()).toRotationMatrix();
-    Eigen::Matrix3d halfInverseRotationMatrix = Eigen::AngleAxisd(-M_PI / 8, Eigen::Vector3d::UnitZ()).toRotationMatrix();
-    Eigen::Vector3d translationVector(1, 2, 3);
-    Eigen::Vector3d inverseTranslationVector(-1, -2, -3);
+    Eigen::Vector3d translationVector(1, 0, 0);
+    Eigen::Vector3d inverseTranslationVector(-std::cos(M_PI/4) , -std::cos(M_PI/4), 0);
 
     Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
     transformationMatrix.block<3, 3>(0, 0) = rotationMatrix;
     transformationMatrix.block<3, 1>(0, 3) = translationVector;
+    Referee::Mapping::ChaslesTransformation transformation(transformationMatrix);
 
     Eigen::Matrix4d inverseTransformationMatrix = Eigen::Matrix4d::Identity();
     inverseTransformationMatrix.block<3, 3>(0, 0) = inverseRotationMatrix;
     inverseTransformationMatrix.block<3, 1>(0, 3) = inverseTranslationVector;
+    Referee::Mapping::ChaslesTransformation inverseTransformation(inverseTransformationMatrix);
 
-    Eigen::Matrix4d halfInverseTransformationMatrix = Eigen::Matrix4d::Identity();
-    halfInverseTransformationMatrix.block<3, 3>(0, 0) = halfInverseRotationMatrix;
-    halfInverseTransformationMatrix.block<3, 1>(0, 3) = Eigen::Vector3d(-0.5, -1.0, -1.5);
     Referee::Mapping::MappingMatrix mappingMatrix(2);
-    mappingMatrix.SetTransformationMatrix(0, 1, transformationMatrix);
-    mappingMatrix.SetTransformationMatrix(1, 0, inverseTransformationMatrix);
-    mappingMatrix.SetTransformationMatrix(0, 0, inverseTransformationMatrix);
-    mappingMatrix.SetTransformationMatrix(1, 1, Eigen::Matrix4d::Identity());
+    mappingMatrix.SetChaslesTransformation(0, 1, transformation);
+    mappingMatrix.SetChaslesTransformation(1, 0, inverseTransformation);
+    mappingMatrix.SetChaslesTransformation(0, 0, inverseTransformation);
+    mappingMatrix.SetChaslesTransformation(1, 1, transformation);
 
     mappingMatrix.CalculateMeanTransformationMatrices();
 
-    if(mappingMatrix.GetMeanTransformationMatrix(0).isApprox(Eigen::Matrix4d::Identity(), 1e-6) && mappingMatrix.GetMeanTransformationMatrix(1).isApprox(halfInverseTransformationMatrix, 1e-6))
+    if(mappingMatrix.GetMeanChaslesTransformation(0).GetRotationAngle() < 1e-6 &&
+       mappingMatrix.GetMeanChaslesTransformation(0).GetTranslation().norm() < 1e-6)
     {
-        std::cout << "Test passed: Mean transformation matrices are correct" << std::endl;
+        std::cout << "Test passed: Mean transformation matrice is correct" << std::endl;
         return 0;
     }
     else
     {
-        std::cout << "Test failed: Mean transformation matrices are not correct" << std::endl;
+        mappingMatrix.GetMeanChaslesTransformation(0).PrintTransformation();
+        std::cout << "Test failed: Mean transformation matrice is not correct" << std::endl;
         return 1;
     }
 
