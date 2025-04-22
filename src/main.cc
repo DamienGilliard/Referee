2238648a328c09032be3f654f8d0db4eace5df21
Damien Gilliard <damien.gilliard@epfl.ch>
2025-04-22 15:46:32 +0200

    feat: implement change of pdf locations in the main

--- src/main.cc
+++ src/main.cc
@@ -4,8 +4,9 @@ int main()
 {
     std::vector<std::string> geolocationFiles = Referee::Utils::FileIterators::GetFilesInDirectory("../../test_files/geolocations", ".petitpoucet");
     std::vector<std::string> plyFileNames = Referee::Utils::FileIterators::GetFilesInDirectory("../../test_files/scans", ".ply");
+    int numberFiles = plyFileNames.size();
 
-    if (plyFileNames.size() != geolocationFiles.size()) 
+    if (numberFiles != geolocationFiles.size()) 
     {
         std::cerr << "Number of geolocation files and ply files do not match" << std::endl;
         return 1;
@@ -19,7 +20,7 @@ int main()
         translationVectors.push_back(translationVector);
     }
 
-    Referee::Mapping::MappingMatrix mappingMatrix(plyFileNames.size());
+    Referee::Mapping::MappingMatrix mappingMatrix(numberFiles);
 
     Eigen::Vector3d meanTranslation =Referee::Transformations::RecenterTranslationVectors(translationVectors);
     std::cout << "Mean translation vector: " << meanTranslation[0] << " " << meanTranslation[1] << " " << meanTranslation[2] << std::endl;
@@ -45,7 +46,6 @@ int main()
     std::cout << "___________________________________________________________" << std::endl;
 
     int count = 0;
-    int totCount = plyFileNames.size();
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr targetPointCloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr coloredFinalPointCloud(new pcl::PointCloud<pcl::PointXYZRGB>);
@@ -79,7 +79,6 @@ int main()
             std::cout << "Downsampling point cloud " << matrix[i][j] << std::endl;
             Referee::Utils::Filtering::VoxelizePointCloud<pcl::PointNormal>(temporarySourcePointCloud, 0.05);
             Referee::Transformations::TranslatePointCloud<pcl::PointNormal>(temporarySourcePointCloud, translationVectors[i]);
-            Referee::Transformations::TranslatePointCloud<pcl::PointNormal>(temporaryTargetPointCloud, -translationVectors[j]);
             Eigen::Matrix4d transformationMatrix = Referee::Mapping::ComputePairwiseTransformation(temporarySourcePointCloud, temporaryTargetPointCloud, Referee::Mapping::TransformationComputationMethod::GlobalMatch);
             Referee::Mapping::ChaslesTransformation chaslesTransformation(transformationMatrix);
             mappingMatrix.SetChaslesTransformation(i, matrix[i][j], chaslesTransformation);
@@ -91,11 +90,66 @@ int main()
     mappingMatrix.CalculateMeanTransformationMatrices();
     mappingMatrix.PrintMeanMatrices();
 
-    for(int i = 0; i < plyFileNames.size(); i++)
+    std::vector<std::tuple<int, double, double>> rotationsPerPC;
+    for(int i = 0; i < numberFiles; i++)
     {
-        std::cout << "mean rotation of pc " << i << " = " << mappingMatrix.GetMeanRotation(i) << std::endl;
-        std::cout << "std dev rotation of pc " << i << " = " << mappingMatrix.GetStdDevRotation(i) << std::endl;
+        rotationsPerPC.push_back(std::make_tuple(i, mappingMatrix.GetMeanRotation(i), mappingMatrix.GetStdDevRotation(i)));
     }
 
+    mappingMatrix.ComputeRotationCoefficients(std::get<1>(rotationsPerPC[0]), 0);
+
+    std::cout << "Probabilities of rotation angles: " << std::endl;
+    for(int i = 0; i < rotationsPerPC.size(); i++)
+    {
+        std::cout << "For Point cloud " << std::get<0>(rotationsPerPC[i]) << std::endl;
+        int indice = 0;
+        double rotationCoefficient = mappingMatrix.GetRotationCoefficient(i, indice);
+        if(rotationCoefficient == 0)
+        {
+            indice = 1;
+            rotationCoefficient = mappingMatrix.GetRotationCoefficient(i, indice);
+        }
+        double p = Referee::Probability::Compute1DProbabilityDensityFunction(rotationCoefficient * mappingMatrix.GetChaslesTransformation(i, indice).GetRotationAngle(), std::get<1>(rotationsPerPC[i]), std::get<2>(rotationsPerPC[i]));
+        std::cout << "Probability of rotation angle: " << p << std::endl;
+    }
+
+    std::vector<double> rotationAngles;
+    std::cout << "angles to be applied: " << std::endl;
+    for(int i = 0; i < numberFiles; i++)
+    {
+        int indice = 0;
+        double angle = mappingMatrix.GetChaslesTransformation(i, indice).GetRotationAngle();
+        if(angle == 0)
+        {
+            indice = 1;
+            angle = mappingMatrix.GetChaslesTransformation(i, indice).GetRotationAngle();
+        }
+        rotationAngles.push_back(angle * mappingMatrix.GetRotationCoefficient(i, indice));
+        std::cout << "Angle to be applied to point cloud " << i << ": " << rotationAngles[i] << std::endl;
+    }
+
+    for(int i = 0; i < numberFiles; i++)
+    {
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr coloredPointCloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+        pcl::io::loadPLYFile(plyFileNames[i], *coloredPointCloud);
+        Referee::Utils::Filtering::VoxelizePointCloud<pcl::PointXYZRGB>(coloredPointCloud, 0.1);
+        for(int j = 0; j < coloredPointCloud->size(); j++)
+        {
+            coloredPointCloud->points[j].r = 255-i*70;
+            coloredPointCloud->points[j].g = i*70;
+            coloredPointCloud->points[j].b = 200 - i*25;
+        }
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr coloredPointCloudTransformed(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+        Eigen::Matrix4d transform = Eigen::Matrix4d::Identity();
+        Eigen::Matrix3d rotationMatrix = Eigen::AngleAxis<double>(rotationAngles[i], Eigen::Vector3d(0, 0, 1)).toRotationMatrix();
+        transform.block<3, 3>(0, 0) = rotationMatrix;
+        transform.block<3, 1>(0, 3) = translationVectors[i];
+        Referee::Transformations::TransformPointCloud<pcl::PointXYZRGB>(coloredPointCloud, transform);
+
+        *coloredFinalPointCloud += *coloredPointCloud;
+    }
+
+    pcl::io::savePLYFile("coloredFinalPointCloud.ply", *coloredFinalPointCloud);
     return 0;
 }
