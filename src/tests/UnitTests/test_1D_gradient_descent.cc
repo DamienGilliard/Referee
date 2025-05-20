821bab5f345a708e0c3d9032247b6860e2a65165
Damien Gilliard <damien.gilliard@epfl.ch>
2025-05-20 21:30:59 +0200

    fix: change parameters of unit test of 1D gradient descent after change to code base

--- src/tests/UnitTests/test_1D_gradient_descent.cc
+++ src/tests/UnitTests/test_1D_gradient_descent.cc
@@ -2,7 +2,7 @@
 
 int main()
 {
-    std::vector<double> result = Referee::Probability::Compute1DGradienDescent({{0, 2}, {0, 1}}, {0.5, 0.5}, 0.1, 1000, 0.002);
+    std::vector<double> result = Referee::Probability::Compute1DGradienDescent({{0, 2}, {0, 1}}, {0.5, 0.5}, 1.0, 1000, 0.00001);
     std::cout << "Result: " << result[0] << ", " << result[1] << std::endl;
     std::cout << "Expected: 0.0, 0.0" << std::endl;
     if (std::abs(result[0] - 0.0) < 0.01 && std::abs(result[1] - 0.0) < 0.01)
