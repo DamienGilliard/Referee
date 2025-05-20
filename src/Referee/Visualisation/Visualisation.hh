64dfb4bd654859ad3ea74785c19f37ec503f4464
Damien Gilliard <damien.gilliard@epfl.ch>
2025-05-20 21:59:18 +0200

    feat: add rendering functions to Visualisation namespace

--- src/Referee/Visualisation/Visualisation.hh
+++ src/Referee/Visualisation/Visualisation.hh
@@ -12,16 +12,29 @@
 #include <pcl/common/centroid.h>
 #include <pcl/io/ply_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
+#include <vtkCamera.h>
 
-namespace FileBasedVisualisation
+namespace Referee::FileBasedVisualisation
 {
     class Visualisation
     {
-    public:
-        Visualisation() = default;
-        ~Visualisation() = default;
-        void VisualisePointCloud(const std::string& plyFilePath);
-        void VisualisePointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
+        public:
+            Visualisation() = default;
+            ~Visualisation() = default;
+            void VisualisePointCloud(const std::string& plyFilePath);
+            void VisualisePointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
+            void ExportImage(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointCloud, const std::string& imagePath);
+            void VisualiseTransformations(std::vector<std::pair<Eigen::Vector3d, Eigen::Matrix4d>> originsAndTransformations, pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointCloud, const std::string& imageFileName = "transformations.png");
+
     };
+
+    /**
+     * @brief Create a coordinate system with the given origin and axis length
+     * 
+     * @param origin The origin of the coordinate system
+     * @param axisLength The length of the axes
+     * @return std::vector<Eigen::Vector3d> The coordinates of the end of axes
+     */
+    std::vector<pcl::PointXYZ> CreateCoordinateSystem(Eigen::Vector3d origin, double axisLength);
 } // FileBasedVisualisation
 
