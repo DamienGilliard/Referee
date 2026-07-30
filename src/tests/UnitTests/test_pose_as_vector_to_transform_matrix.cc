930841b6692ee40a98a22e3585a2a821bf71f659
DamienGilliard <damien.gilliard@epfl.ch>
2026-07-30 11:02:55 +0200

    feat: update test of vector to matrix conversion for poses

--- src/tests/UnitTests/test_pose_as_vector_to_transform_matrix.cc
+++ src/tests/UnitTests/test_pose_as_vector_to_transform_matrix.cc
@@ -3,12 +3,12 @@
 int main()
 {
     Eigen::Matrix<double, 6, 1> poseAsVector;
-    poseAsVector[0] = 1.0;
-    poseAsVector[1] = 1.0;
-    poseAsVector[2] = 1.0;
-    poseAsVector[3] = 0.0;
-    poseAsVector[4] = 0.0;
-    poseAsVector[5] = 0.0;
+    poseAsVector[0] = 0.0;
+    poseAsVector[1] = 0.0;
+    poseAsVector[2] = 0.0;
+    poseAsVector[3] = 1.0;
+    poseAsVector[4] = 1.0;
+    poseAsVector[5] = 1.0;
 
     Eigen::Matrix4d transformationMatrix = Referee::Utils::Conversions::poseAsVectorToTransformationMatrix(poseAsVector);
     std::cout << "Transformation matrix:\n" << transformationMatrix << std::endl;
@@ -18,13 +18,13 @@ int main()
         return 1;
     }
 
-    poseAsVector[5] = M_PI / 2; // 90 degrees rotation around Z axis
+    poseAsVector[2] = M_PI / 2; // 90 degrees rotation around Z axis
 
     Eigen::Matrix4d translationMatrix = Eigen::Matrix4d::Identity();
     translationMatrix(0, 3) = 1.0;
     translationMatrix(1, 3) = 1.0;
     translationMatrix(2, 3) = 1.0;
-    Eigen::Matrix3d rotationMatrix = Eigen::AngleAxisd(poseAsVector[5], Eigen::Vector3d::UnitZ()).toRotationMatrix();
+    Eigen::Matrix3d rotationMatrix = Eigen::AngleAxisd(poseAsVector[2], Eigen::Vector3d::UnitZ()).toRotationMatrix();
     Eigen::Matrix4d expectedRotationMatrix = Eigen::Matrix4d::Identity();
     expectedRotationMatrix.block<3, 3>(0, 0) = rotationMatrix;
     
